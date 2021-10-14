3c4cea38dcae36ef8d7a71ab18e183f31d20cdbb
Doetrp <terapat98.t@gmail.com>
2021-10-14 10:34:27 +0700

    m

--- src/pages/Aboutme/Aboutme.jsx
+++ src/pages/Aboutme/Aboutme.jsx
@@ -1,19 +1,27 @@
 import "./Aboutme.css";
-import { Card } from 'antd';
+import { Card } from "antd";
 
 export default function Aboutme() {
   return (
     <div>
-      <Card className="TextHeader" title="About Me" bordered={false} style={{ width: 500 }}>
-      <div className="inBody">
-        <span>Name : </span>
-        <label> Terapat Thuacharoen</label>
-        <br />
-        <label>Nick Name :  Doe</label><br />
-        <label>lineid :   Doe_trp  </label><br />
-        <label>tel :   091-xxx-xxxx  </label><br />
-      </div>
-    </Card>      
+      <Card
+        className="TextHeader"
+        title="About Me"
+        bordered={false}
+        style={{ width: 500 }}
+      >
+        <div className="inBody">
+          <span>Name : </span>
+          <label> Terapat Thuacharoen</label>
+          <br />
+          <label>Nick Name : Doe</label>
+          <br />
+          <label>lineid : Doe_trp </label>
+          <br />
+          <label>tel : 091-xxx-xxxx </label>
+          <br />
+        </div>
+      </Card>
     </div>
   );
 }
