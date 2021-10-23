21f80feeb91954dedb6a11169bdd48f9b52671d4
Doetrp <terapat98.t@gmail.com>
2021-10-23 09:01:44 +0700

    start

--- src/pages/Aboutme/Aboutme.jsx
+++ src/pages/Aboutme/Aboutme.jsx
@@ -1,25 +1,52 @@
 import "./Aboutme.css";
-import { Card } from "antd";
+import { Card, Button, Input, Form } from "antd";
 
 export default function Aboutme() {
   return (
-    <div>
+    <div style={{ textItem: "center" }}>
       <Card
         className="TextHeader"
-        title="About Me"
+        title="รายรับ - รายจ่าย"
         bordered={false}
         style={{ width: 500 }}
       >
-        <div className="inBody">
-          <span>Name : </span>
-          <label> Terapat Thuacharoen</label>
-          <br />
-          <label>Nick Name : Doe</label>
-          <br />
-          <label>lineid : Doe_trp </label>
-          <br />
-          <label>tel : 091-xxx-xxxx </label>
-          <br />
+        <div>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8
+            }}
+            >
+            <Form.Item
+              label="รายการ"
+              name="รายการ"
+              rules={[
+                {
+                  required: true,
+                  message: "กรุณาระบุรายการด้วยครับ!",
+                },
+              ]}
+            >
+              <Input type="text" placeholder="รายการ" />
+            </Form.Item>
+            <Form.Item
+              label="จำนวนเงิน"
+              name="จำนวนเงิน"
+              rules={[
+                {
+                  required: true,
+                  message: "กรุณากรอกจำนวนเงินด้วยครับ!",
+                },
+              ]}
+            >
+              <Input type="number" placeholder="จำนวนเงิน" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" shape="round">
+                submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </Card>
     </div>
