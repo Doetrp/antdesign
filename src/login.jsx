eed55044f5b0c9cc0e22dee7c79b59c785fa2627
Doetrp <terapat98.t@gmail.com>
2021-11-26 16:37:40 +0700

    firt login

---
+++ src/login.jsx
@@ -0,0 +1,13 @@
+import { Card, Button, Input, Form, Checkbox } from "antd";
+
+export default function login() {
+  return (
+    <Card
+      title="รายรับ - รายจ่าย"
+      bordered={false}
+      type="inner"
+      className="ant-card"
+      style={{ background: "white" }}
+    ></Card>
+  );
+}
--- src/subpage/Subrapper/Eminem.jsx
+++ src/subpage/Subrapper/Eminem.jsx
@@ -1,12 +1,20 @@
 import "./Subeminem.css";
 import MainLayout from "../../common/MainLayout";
+import { Row, Col } from "antd";
 export default function Eminem() {
   return (
     <MainLayout>
-      <div>
-        <div className="subEminemHeaders">
-          <label> Eminem </label>
-        </div>
+      <div className="subEminemHeaders">
+        <h1> Eminem </h1>
+      </div>
+      <div className="subEminemBodyfont"> 
+        <Row>
+          <Col span={1} />
+          <Col span={22} style={{ textAlign: "left", fontSize: "20px" }}>
+            vm,fkpvmfkpmv
+          </Col>
+          <Col span={1} />
+        </Row>
       </div>
     </MainLayout>
   );
